99d027edc0682c460ce2a67ef3db75def83ceb02
Abhijeet_15 <118336271+AbhijeetKamalekar15@users.noreply.github.com>
2025-02-04 20:30:06 +0530

    Changes: Completed create interview page UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import PublicLayout from './layouts/public-layout';
 import HomePage from './routes/home';
 import AuthenticationLayout from './layouts/auth-layout';
@@ -6,6 +6,9 @@ import SignInPage from './routes/signin';
 import SignUpPage from './routes/signup';
 import ProtectedRoutes from './layouts/protected-layout';
 import MainLayout from './layouts/main-layout';
+import { Generate } from './components/generate';
+import { Dashboard } from './routes/dashboard';
+import { CreateEditPage } from './routes/create-edit-page';
 
 
 
@@ -14,19 +17,25 @@ const App = () => {
     <Router>
       <Routes>
         {/* public routes */}
-        <Route element={<PublicLayout/>}>
-        <Route index element={<HomePage/>}/>
+        <Route element={<PublicLayout />}>
+          <Route index element={<HomePage />} />
         </Route>
 
         {/* authentication layout  */}
-        <Route element={<AuthenticationLayout/>}>
-        <Route path="/signin/*" element={<SignInPage/>}/>
-        <Route path="/signup/*" element={<SignUpPage/>}/>
+        <Route element={<AuthenticationLayout />}>
+          <Route path="/signin/*" element={<SignInPage />} />
+          <Route path="/signup/*" element={<SignUpPage />} />
         </Route>
 
         {/* protected routes  */}
-        <Route element={<ProtectedRoutes><MainLayout/></ProtectedRoutes>}>
-        
+        <Route element={<ProtectedRoutes><MainLayout /></ProtectedRoutes>}>
+          {/* add all the protected routes */}
+          <Route path='/generate' element={<Generate />}>
+            <Route index element={<Dashboard />} />
+            <Route path=':interviewId' element={<CreateEditPage/>}/>
+          </Route>
+
+
         </Route>
       </Routes>
     </Router>
