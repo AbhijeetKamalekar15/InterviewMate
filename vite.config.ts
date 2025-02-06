08c044c5e86744558ec43d753db8a1ed01c9e662
Abhijeet_15 <118336271+AbhijeetKamalekar15@users.noreply.github.com>
2025-02-06 16:31:54 +0530

    done2

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: "0.0.0.0",
     port: Number(process.env.VITE_PORT) || 3000, // Ensure it's a number
   },
   resolve: {
